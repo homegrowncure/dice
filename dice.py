1a1906287e9f524e62855af2201eabde035dbaf7
ebolakat <ebolakat@tuta.io>
2015-08-16 15:22:27 -0400

    First

---
+++ dice.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Usage: dice.py [numwords=6]
+
+import sys
+
+WORDLIST = 'dicewords'
+
+def roll():
+    with open('/dev/urandom') as rand:
+        return ord(rand.read(1)) % 6 + 1
+
+def word():
+    combo = []
+    for x in range(0, 5):
+        combo.append(roll())
+
+    with open(WORDLIST) as words:
+        lines = words.readlines()
+        for l in lines:
+            if l[:5] == ''.join(str(x) for x in combo):
+                return l[6:len(l) - 1]
+
+for x in range(0, int(sys.argv[1]) if len(sys.argv) > 1 else 6):
+    print word(),
